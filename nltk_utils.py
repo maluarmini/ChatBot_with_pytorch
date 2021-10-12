6f880c06675531e14092965036f50a0d96078a92
maluarmini <luiza.armini@gmail.com>
2021-10-12 10:40:47 -0300

    Code and data

---
+++ nltk_utils.py
@@ -0,0 +1,33 @@
+import numpy as np
+import nltk
+# nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+#   Dividir a palavra em tokens
+
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+#   Encontrar a raiz da palavra = stemming
+    return stemmer.stem(word.lower())
+
+
+def bag_of_words(tokenized_sentence, words):
+    """
+    retorna o array de bag of words
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    # stem each word
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
+            bag[idx] = 1
+
+    return bag
